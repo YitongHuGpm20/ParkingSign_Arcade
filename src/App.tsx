8a41bcc57bcca6b8867d015be4d03fc3462bd737
YitongHuGpm20 <yhu216@ucsc.edu>
2025-12-13 18:14:14 -0800

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app-root">
+            <h1 className="title">ParkingSign Arcade</h1>
+            <p className="subtitle">Read the signs. Park or pay.</p>
+        </div>
+    );
+}
+
+export default App;
