00f35eb8fe9d68e1e4cdfcd4a2d92027e8a1e496
YitongHuGpm20 <yhu216@ucsc.edu>
2025-12-14 20:01:24 -0800

    Adjusted main menu layout

--- src/App.tsx
+++ src/App.tsx
@@ -24,23 +24,25 @@ function App() {
             {import.meta.env.DEV && <DebugFrame/>}
             
             <div className="app-root">
-                {/* ====== Title ====== */}
-                <h1 id="title-l1" className="title">ParkingSign</h1>
-                <h1 id="title-l2" className="title">Arcade</h1>
+                <div className="ui-layer">
+                    {/* ====== Title ====== */}
+                    <h1 id="title-l1" className="title">ParkingSign</h1>
+                    <h1 id="title-l2" className="title">Arcade</h1>
 
-                {/* ====== Main Menu ====== */}
-                <div className="main-menu">
-                    <button className="main-menu-btn" onClick={() => openModeSelect()}>
-                        PLAY
-                    </button>
-                    <button className="main-menu-btn" onClick={() => openSettings()}>
-                        SETTINGS
-                    </button>
-                    <button className="main-menu-btn" onClick={() => openCredits()}>
-                        CREDITS
-                    </button>
+                    {/* ====== Main Menu ====== */}
+                    <div className="main-menu">
+                        <button className="main-menu-btn" onClick={() => openModeSelect()}>
+                            PLAY
+                        </button>
+                        <button className="main-menu-btn" onClick={() => openSettings()}>
+                            SETTINGS
+                        </button>
+                        <button className="main-menu-btn" onClick={() => openCredits()}>
+                            CREDITS
+                        </button>
+                    </div>
                 </div>
-                
+
                 <CarAnim />
             </div>
         </>
